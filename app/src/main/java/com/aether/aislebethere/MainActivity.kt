9d3c8d014a7f8cced3ed1184665817e68e4df6ad
Matthew Bowden <bowdenm@spu.edu>
2017-09-12 20:34:06 -0700

    Working style and onKey for text field

--- app/src/main/java/com/aether/aislebethere/MainActivity.kt
+++ app/src/main/java/com/aether/aislebethere/MainActivity.kt
@@ -2,11 +2,28 @@ package com.aether.aislebethere
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Item selectors
+        val newItemEditText: EditText = findViewById(R.id.newItemEditText)
+        newItemEditText.setOnKeyListener(object: View.OnKeyListener{
+            override fun onKey(v: View?, code: Int, ev: KeyEvent?) : Boolean {
+                if (ev?.action == KeyEvent.ACTION_UP && (ev.keyCode == KeyEvent.KEYCODE_ENTER || code == EditorInfo.IME_ACTION_DONE)) {
+                    Toast.makeText(this@MainActivity, "typed", Toast.LENGTH_SHORT).show()
+                }
+                return false
+            }
+        })
     }
+
 }
